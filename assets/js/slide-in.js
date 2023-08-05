b190de0cec628ce101c1888479ae52fdd008a27c
rsdrew <rsdrew@umich.edu>
2023-08-05 13:07:41 -0400

    Minor fixes including: fixed broken button hover. Removed js logging.

--- assets/js/slide-in.js
+++ assets/js/slide-in.js
@@ -3,22 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function slideInElements() {
         elements.forEach(element => {
-            const elementOffset = element.getBoundingClientRect().top;
-            const elementHeight = element.clientHeight;
-
-            if (element.id == 'mailing-list-sub-header') {
-                console.log(`window.scrollY: ${window.scrollY}`);
-
-                console.log(`elementOffset: ${elementOffset}`);
-                console.log(`window.innerHeight: ${window.innerHeight}`);
-                console.log(`elementHeight: ${elementHeight}`);
-                
-                console.log(`output: ${elementOffset - window.innerHeight / 10}`)
-    
-                console.log('');
-            }
-
-            if (elementOffset < window.innerHeight - 10) {
+            if (element.getBoundingClientRect().top < window.innerHeight - 10) {
                 element.classList.add('slide-in--activate');
             }
         })
